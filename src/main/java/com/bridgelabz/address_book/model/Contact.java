4fcc523b85be60e93366dde28e7e8e57d45f325f
pallavi15 <pallavigaharwar15@gmail.com>
2025-03-05 02:04:13 +0530

    [Pallavi] Add . Adding model, Repository and controller in address Book

---
+++ src/main/java/com/bridgelabz/address_book/model/Contact.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.address_book.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+
+}
